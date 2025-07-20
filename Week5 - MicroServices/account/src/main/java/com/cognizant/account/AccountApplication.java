3fe339342dffde1450c852af5096e14707c183b8
tejuaswene <125335142+tejuaswene@users.noreply.github.com>
2025-07-20 21:35:32 +0530

    Add files via upload

---
+++ Week5 - MicroServices/account/src/main/java/com/cognizant/account/AccountApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+}
